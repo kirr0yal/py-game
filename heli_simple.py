d3695e5926790c7da5e59b00829e7a3fc1b0519b
kirr0yal <adilyessil@gmail.com>
2023-05-29 13:52:26 +0600

    Heli first commit

---
+++ heli_simple.py
@@ -0,0 +1,174 @@
+import pygame
+
+pygame.init()
+size = (700, 500)
+screen_width = size[0]
+screen_height = size[1]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Heli")
+clock = pygame.time.Clock()
+
+heli_right = pygame.image.load("images/heli_right.png")
+heli_right = pygame.transform.scale(heli_right, (screen_width // 10, screen_width // 10))
+heli_left = pygame.image.load("images/heli_left.png")
+heli_left = pygame.transform.scale(heli_left, (screen_width // 10, screen_width // 10))
+
+background_image = pygame.image.load("images/plx-5.png")
+background_image = pygame.transform.scale(background_image, size)
+
+x = screen_width // 2 - screen_width // 20
+y = screen_height // 2 - screen_height // 20
+
+dx = 0
+dy = 0
+heading_right = True
+flying_up = False
+
+background_x = 0
+
+level = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1],
+    [1, 0, 3, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 1, 1, 1, 1, 1, 0, 0, 2, 1, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 2, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+platforms = list()
+platforms2 = list()
+
+brick_size = size[1] // 10
+
+for i in range(len(level)):
+    for j in range(len(level[i])):
+        if level[i][j] == 1:
+            platforms.append(pygame.Rect(j*brick_size, i*brick_size, brick_size, brick_size))
+        if level[i][j] == 2:
+            platforms2.append(pygame.Rect(j*brick_size, i*brick_size, brick_size, brick_size))
+        if level[i][j] == 3:
+            x, y = j*brick_size, i*brick_size
+
+fire = False
+fire_x, fire_y = 50, 50
+fire_dx = 10
+fire_dy = 0
+fire_counter = 0
+
+game_over = False
+while not game_over:
+    if flying_up:
+        dy -= 1
+    else:
+        dy += 0.5
+        if dy > 10:
+            dy = 10
+
+    x += dx
+    heli_rect = pygame.Rect(x, y, screen_width//10, screen_width//12)
+    for platform in platforms + platforms2:
+        if heli_rect.colliderect(platform):
+            x -= dx
+
+    y += dy
+    heli_rect = pygame.Rect(x, y, screen_width // 10, screen_width // 12)
+    for platform in platforms + platforms2:
+        if heli_rect.colliderect(platform):
+            y -= dy
+            dy = 0
+
+    if x + background_x > size[0] * 3/4:
+        background_x = size[0] * 3/4 - x
+
+    if x + background_x < size[0] * 1/4:
+        background_x = size[0] * 1/4 - x
+
+    if fire:
+        fire_counter -= 1
+        if fire_counter <= 0:
+            fire = False
+        fire_x += fire_dx
+        fire_y += fire_dy
+        for platform in platforms:
+            if platform.collidepoint(fire_x, fire_y):
+                fire = False
+        for platform in platforms2:
+            if platform.collidepoint(fire_x, fire_y):
+                fire = False
+                platforms2.remove(platform)
+                break
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                dx = 7
+                heading_right = True
+            if event.key == pygame.K_LEFT:
+                dx = -7
+                heading_right = False
+            if event.key == pygame.K_UP:
+                flying_up = True
+            if event.key == pygame.K_DOWN:
+                dy = 7
+            if event.key == pygame.K_SPACE:
+                if not fire:
+                    fire = True
+                    fire_x = x + screen_width//20
+                    fire_y = y + screen_width//20
+                    fire_counter = 40
+                    if heading_right:
+                        fire_dx = 10
+                    else:
+                        fire_dx = -10
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                if dx > 0:
+                    dx = 0
+            if event.key == pygame.K_LEFT:
+                if dx < 0:
+                    dx = 0
+            if event.key == pygame.K_UP:
+                flying_up = False
+            if event.key == pygame.K_DOWN:
+                if dy > 0:
+                    dy = 0
+
+    screen.fill((255, 255, 255))
+    screen.blit(background_image, (background_x % size[0], 0))
+    screen.blit(background_image, (background_x % size[0] - size[0], 0))
+
+    for platform in platforms:
+        pygame.draw.rect(screen, (0, 120, 0), (
+            platform.x + background_x,
+            platform.y,
+            platform.width,
+            platform.height
+        ))
+
+    for platform in platforms2:
+        pygame.draw.rect(screen, (150, 250, 105), (
+            platform.x + background_x,
+            platform.y,
+            platform.width,
+            platform.height
+        ))
+
+    if heading_right:
+        screen.blit(heli_right, (x + background_x, y))
+    else:
+        screen.blit(heli_left, (x + background_x, y))
+
+    if fire:
+        pygame.draw.circle(screen, (120, 0, 0), (fire_x + background_x, fire_y), 5)
+
+    pygame.display.flip()
+    clock.tick(25)
+
+pygame.quit()
